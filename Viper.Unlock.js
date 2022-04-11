0295292c30c1de478eb3e4ac72a917b2b69d051f
Maurice van 't Loo <maurice@mvantloo.nl>
2022-04-11 22:13:37 +0200

    Commit 2022-04-11 22:13:37

--- Viper.Unlock.js
+++ Viper.Unlock.js
@@ -16,11 +16,13 @@ console.log(ct.displayDateTime(Date.now()))
 
 async function main() {  
   await ct.connect_provider_signer("harmonypokt")  // Connect to Harmony and show ONE balance
-  await ct.viper_show()                            // Show VIPER balance
-  unlocked = await ct.viper_unlock()               // Unlock the VIPER coins, returns the amount of unlocked VIPER
-  balance = await ct.viper_show()                  // Show VIPER balance, returns the amount of VIPER
-  await ct.viper_swap(balance)                     // Swap the VIPER to ONE, choose if you want to swap what is unlocked, or VIPER balance
-  await ct.one_show()                              // Show ONE balance after the Swap
+//  await ct.viper_show()                            // Show VIPER balance
+//  unlocked = 
+await ct.viper_unlock()               // Unlock the VIPER coins, returns the amount of unlocked VIPER
+await ct.swap("VIPERSWAP", "VIPER", "ONE", "ALL")
+//balance = await ct.viper_show()                  // Show VIPER balance, returns the amount of VIPER
+//  await ct.viper_swap(balance)                     // Swap the VIPER to ONE, choose if you want to swap what is unlocked, or VIPER balance
+//  await ct.one_show()                              // Show ONE balance after the Swap
 }
 
 main()
