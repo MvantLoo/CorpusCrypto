ce3358f05b26c7cd4de38c0ac6eff38868377205
Maurice van 't Loo <maurice@mvantloo.nl>
2022-04-25 19:59:45 +0200

    Commit 2022-04-25 19:59:45

---
+++ jes-Viper.Unlock.js
@@ -0,0 +1,28 @@
+/*
+  ViperSwap Unlock locked tokens
+  https://viper.exchange/#/staking/unlock
+
+  Version: ALPHA 3 - 2022-03-22
+
+*/
+
+let CT = require("./CorpusTools")
+let ct = new CT.CorpusTools(wallet.yezzi.json)
+
+console.log(ct.GREEN,'\n############################')
+console.log(ct.GREEN,'#####   Viper.Unlock   #####')
+console.log(ct.GREEN,'############################\n')
+console.log(ct.displayDateTime(Date.now()))
+
+async function main() {  
+  await ct.connect_provider_signer("harmonypokt")  // Connect to Harmony and show ONE balance
+//  await ct.viper_show()                            // Show VIPER balance
+//  unlocked = 
+await ct.viper_unlock()               // Unlock the VIPER coins, returns the amount of unlocked VIPER
+await ct.swap("VIPERSWAP", "VIPER", "ONE", "ALL")
+//balance = await ct.viper_show()                  // Show VIPER balance, returns the amount of VIPER
+//  await ct.viper_swap(balance)                     // Swap the VIPER to ONE, choose if you want to swap what is unlocked, or VIPER balance
+//  await ct.one_show()                              // Show ONE balance after the Swap
+}
+
+main()
